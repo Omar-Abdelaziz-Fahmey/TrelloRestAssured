0a106c2686de360dbb200ebc1866c8309de93709
Omar-Abdelaziz-Fahmey <omar.ragh37@gmail.com>
2025-12-09 22:17:11 +0200

    feat: Add E2ETestRunner

---
+++ src/test/java/Options/E2ETestRunner.java
@@ -0,0 +1,16 @@
+package Options;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/java/features",
+        glue = {"stepDefinitions"},
+        plugin = {"pretty", "json:target/jsonReports/e2e-report.json"},
+        tags = "@E2E"
+)
+public class E2ETestRunner {
+
+}
