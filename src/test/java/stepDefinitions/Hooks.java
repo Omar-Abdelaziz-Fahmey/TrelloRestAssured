a30db60b9bdcf1004d687276fbbb0f6dedf51955
Omar <omar.ragh37@gmail.com>
2026-03-03 21:42:23 +0200

    Add AfterAll hook to clean up boards before running tests

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,7 +1,16 @@
 package stepDefinitions;
 
 
-public class Hooks {
+import io.cucumber.java.AfterAll;
 
+import java.io.IOException;
+
+public class Hooks {
+    static StepDefinition s = new StepDefinition();
+
+    @AfterAll
+    public static void cleanup() throws IOException {
+        s.deleteAllBoards(); //delete all boards before running the tests to ensure a clean state
+    }
 
 }
